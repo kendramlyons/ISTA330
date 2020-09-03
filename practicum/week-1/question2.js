dc5e161effdfcff98537412626f2ccae29f5ba6e
Kendra Lyons <KLyons@kendra-lyonss-macbook-pro.local>
2020-09-03 11:29:34 -0700

    q2attempt

--- practicum/week-1/question2.js
+++ practicum/week-1/question2.js
@@ -18,5 +18,7 @@ Example: input: cookies = [3, 5, 8] , extraCookies = 8
     if (totalCookies%cookies.length != 0){
         return false;
         }
-    return true; 
+    else if (totalCookies%cookies.length === 0){
+        return true; 
+    }
  };
