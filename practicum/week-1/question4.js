61adf4564f7f8fe7259427b0f2a6c2caa18c1f1c
Kendra Lyons <KLyons@kendra-lyonss-macbook-pro.local>
2020-09-03 11:52:16 -0700

    q4attempt

--- practicum/week-1/question4.js
+++ practicum/week-1/question4.js
@@ -11,5 +11,14 @@ The letters in A are guaranteed distinct, and all characters in A and B are lett
 */
 
 var howManyCommon = function(A, B) {
+    repeats = 0
+    for(i=0; i<A.length; i++){
+        for(j=0; j<B.length; j++){
+            if(A[i]===B[j]){
+                repeats+=1
+}
+}
+    return repeats
+}
 
 };
