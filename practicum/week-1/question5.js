a28127f3078d0781367756b21b138e70a218df32
Kendra Lyons <KLyons@kendra-lyonss-macbook-pro.local>
2020-09-04 13:57:32 -0700

    q5attempt1

--- practicum/week-1/question5.js
+++ practicum/week-1/question5.js
@@ -12,7 +12,22 @@ output: 3
 */
 
 var minimalReduction = function(n) {
-
+    var num_ops = 0;
+    while (n != 0){
+        if (n % 4 == 0){
+            n = n/4;
+            num_ops+=1;
+        }
+        if (n % 3 == 0){
+            n = n/3;
+            num_ops+=1;
+        }
+        if (n > 0){
+            n = n-1;
+            num_ops+=1
+        }
+    return num_ops;
+    }
 };
 
 console.log('hi');
