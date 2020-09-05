ffb19f4d60e21e81416bcef9259a9f6b41949baa
Kendra Lyons <KLyons@kendra-lyonss-macbook-pro.local>
2020-09-05 14:59:27 -0700

    q6();

--- practicum/week-1/question6.js
+++ practicum/week-1/question6.js
@@ -7,14 +7,14 @@ output: 3
 
 var median = function(input) {
     if (input.length % 2 == 1) {
-        var medIndex = input.length - (input.length - 1)/2; 
+        var medIndex = input.length - ((input.length - 1)/2); 
         return input[medIndex];
     }
     else if (input.length % 2 == 0) {
-        var mp1 = input.length/2; // 6/2 = 3
-        var mp2 = mp1 + 1; // 3+1 = 4
+        var mp1 = input.length/2; // 8/2 = 4
+        var mp2 = mp1 + 1; // 4+1 = 5
         var medValue = (input[mp1]+input[mp2])/2
-        return medValue
+        return medValue;
     }
 
     // for (i=0; i<input.length; i++) {
