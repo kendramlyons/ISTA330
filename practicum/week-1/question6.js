d7a68a5ea68ea4666d4578a6e09a7f27f02010b8
Kendra Lyons <KLyons@kendra-lyonss-macbook-pro.local>
2020-09-05 15:22:59 -0700

    q6eventestch

--- practicum/week-1/question6.js
+++ practicum/week-1/question6.js
@@ -6,16 +6,19 @@ output: 3
 */
 
 var median = function(input) {
-    //test for odd length
+    //test for odd length SUCCESS
     if (input.length % 2 == 1) {
         var medIndex = (input.length - 1)/2; // (7-1)/2 = 3
         return input[medIndex];
     }
     //test for even length
     else if (input.length % 2 == 0) {
-        var mp1 = input[input.length / 2]; // 8/2 = 4
-        var mp2 = input[mp1 - 1]; // 4-1 = 3
-        var medValue = (mp1+mp2)/2;
+        var mp1 = (input.length / 2) - 1;
+        var mp2 = (input.length / 2);
+        var medValue = (index[mp1] + index[mp2]) / 2;
+        //var mp1 = input[input.length / 2]; // 8/2 = 4
+        // var mp2 = input[mp1 - 1]; // 4-1 = 3
+        // var medValue = (mp1+mp2)/2;
         return medValue;
     }
 };
