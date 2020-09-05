7f81f5ba51f9c59fd9bd33d24cd109a7ea5281b6
Kendra Lyons <KLyons@kendra-lyonss-macbook-pro.local>
2020-09-05 15:14:45 -0700

    q6fixoddtest

--- practicum/week-1/question6.js
+++ practicum/week-1/question6.js
@@ -8,14 +8,14 @@ output: 3
 var median = function(input) {
     //test for odd length
     if (input.length % 2 == 1) {
-        var medIndex = input.length - ((input.length - 1)/2); 
+        var medIndex = (input.length - 1)/2; // (7-1)/2 = 3
         return input[medIndex];
     }
     //test for even length
     else if (input.length % 2 == 0) {
-        var mp1 = input.length / 2; // 8/2 = 4
-        var mp2 = mp1 - 1; // 4-1 = 3
-        var medValue = (input[mp1]+input[mp2])/2;
+        var mp1 = input[input.length / 2]; // 8/2 = 4
+        var mp2 = input[mp1 - 1]; // 4-1 = 3
+        var medValue = (mp1+mp2)/2;
         return medValue;
     }
 };
