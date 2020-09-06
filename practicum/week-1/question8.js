ee98e765eb271d6068b89f202ed114694ae95ea9
Kendra Lyons <KLyons@kendra-lyonss-macbook-pro.local>
2020-09-05 21:49:20 -0700

    q8

--- practicum/week-1/question8.js
+++ practicum/week-1/question8.js
@@ -8,10 +8,10 @@ output: [1, 0, 1, 2]
 */
 var biggerAndEven = function(input) {
     var evenAndBigger = [];
-    for(i=0; i<input.length; i++) {    
+    for(i=0; i<input.length; i++) {
         var howMany = 0;
-        for(j=0; j<input.length; j++){
-            if( (input[i] < input[j]) && (input[j]%2)==0 ){
+        for(j=0; j<input.length; j++) {
+            if( (input[i] < input[j]) && (input[j]%2)==0 ) {
                 howMany += 1;
             }
         }
