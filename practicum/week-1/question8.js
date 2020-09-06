3a921bd1427c1ce6d80cd34bbd5f1731d1718b27
Kendra Lyons <KLyons@kendra-lyonss-macbook-pro.local>
2020-09-05 18:39:21 -0700

    q8attempt1

--- practicum/week-1/question8.js
+++ practicum/week-1/question8.js
@@ -6,7 +6,16 @@ Example:
 input: [23, 44, 12, 4]
 output: [1, 0, 1, 2]
 */
-
+var evenAndBigger = [];
+var howMany = 0;
 var biggerAndEven = function(input) {
-
+    for(i=0; i<input.length; i++) {    
+        for(j=0; j<input.length; j++){
+            if(input[i] < input[j] && input[j]%2==0){
+                howMany++;
+            }
+        evenAndBigger.push(howMany);
+        }
+    }
+    return evenAndBigger;
 };
