0466380ca54b1190bfaeaa4ffe1ac2721f0f1096
Kendra Lyons <KLyons@kendra-lyonss-macbook-pro.local>
2020-09-07 23:18:10 -0700

    q10toString

--- practicum/week-1/question10.js
+++ practicum/week-1/question10.js
@@ -4,10 +4,16 @@ Given an integer n, return difference between the maximum and the minimum of its
 Example:
 input: 472
 output: 7 - 2 = 5
+
+    var nArray = (n.toString().split('')).sort();
+    var min = nArray[0];
+    var max = nArray[(nArray.length - 1)];
+    var mmm = (max - min);
+    return mmm;
 */
 
 var maxMinusMin = function(n) {
-    var nArray = (str(n).split()).sort();
+    var nArray = (n.toString().split('')).sort();
     var min = nArray[0];
     var max = nArray[(nArray.length - 1)];
     var mmm = (max - min);
