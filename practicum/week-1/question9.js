300d6749ad9e1b00bf0e8a05f8b9ac80aec7e518
Kendra Lyons <KLyons@kendra-lyonss-macbook-pro.local>
2020-09-07 23:56:51 -0700

    q9

--- practicum/week-1/question9.js
+++ practicum/week-1/question9.js
@@ -5,19 +5,7 @@ We want to shuffle the string based on the shufleIndices array.
 Example:
 input: 'llheo', shuffleIndices = [2, 3, 0, 1, 4]
 output: 'hello'
-    // var sortIndices = shuffleIndices.sort();
-    // var chArray = [];        
-    // chArray.push(input[shuffleIndices[i]]);
-    // for(i=0; i<chArray.length; i++) {
-    // var i = unshuffled.length;
-    // while (i<input.length) {
-    //     if (shuffleIndices[i] == i) {
-    //         unshuffled.push(input[i]);
-    //         break;
-    //     }
-    //     i++;
-    // }
-    // return unshuffled;
+
 */
 
 var suffleString = function(input, shuffleIndices) {
