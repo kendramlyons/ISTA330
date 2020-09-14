9eb682da70f4027c810ff0c6e5cf50d712d1ce2f
Kendra Lyons <KLyons@kendra-lyonss-macbook-pro.local>
2020-09-13 19:06:55 -0700

    q2

--- practicum/week-2/question2.js
+++ practicum/week-2/question2.js
@@ -10,14 +10,18 @@ Example: input: [-12,3,-1,5,-2,1,-7]
           */
 
  var largestSubarray = function(input) {
-    var big_sum;
-    var running_sum;
+    var big_sum = 0;
+    var running_sum = 0;
     for(i=0; i<input.length; i++) {
-        big_sum = input[i];
-        for(j=1; j < input.length; j++) {
-            running_sum = input[i] + input[j]
+        big_sum += input[i];
+        running_sum += input[i]
+        for(j=1; j < (input.length-1); j++) {
+            running_sum += input[j]
             if (running_sum > big_sum) {
                 big_sum = running_sum;
+            }
+            else {
+                break
             }
         }
     }
