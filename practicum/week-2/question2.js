974fb663a40ae4a4fb6313822b9a09b3eda21280
Kendra Lyons <KLyons@kendra-lyonss-macbook-pro.local>
2020-09-13 19:20:17 -0700

    q2else

--- practicum/week-2/question2.js
+++ practicum/week-2/question2.js
@@ -20,6 +20,9 @@ Example: input: [-12,3,-1,5,-2,1,-7]
             if (running_sum > big_sum) {
                 big_sum = running_sum;
             }
+            else {
+                break
+            }
         }
     }
     return big_sum;
