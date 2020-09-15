a5006df5aa90ba5b555d7360967552622e5a997c
Kendra Lyons <KLyons@kendra-lyonss-macbook-pro.local>
2020-09-14 21:28:42 -0700

    q3Array()

--- practicum/week-2/question3.js
+++ practicum/week-2/question3.js
@@ -11,35 +11,24 @@ output: [
   [1,3,3,1] 
 ]
 
-        if (n = 1) {
-            result.push([1]);
-        } else if (n = 2) {
-            result.push([1],[1,1]);
-        } else if (n > 2) {
-            result.push([1],[1,1]);
-            for ( j = 0; j < (n - 2); j++) {
-                var numRows = n - 2; 
-                var rows = [] * numRows;
-                result.concat(rows)
-            }
-        }
-
 help from David Fimbres 
 */
 
 var PascalTriangle = function(n) {
-    var result = []
+    var result = new Array();
     for (i = 0; i < n; i++) {
         result.push([]);
-        for (j = 0; j < n; j++) {
+        for (j = 0; j < (n-1); j++) {
             if (j == 0 || i == 0 || i == 1) {
                 result[i][j].push(1);
             }
             else {
-                var pSum = num(result[i-1][j-1]) + num(result[i-1][j])
+                var pSum = result[i-1][j-1] + result[i-1][j];
                 result[i][j].push(pSum);
             }
         }
     }
     return result;
 };
+let test = PascalTriangle(4);
+console.log(test);
