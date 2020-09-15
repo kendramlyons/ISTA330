26b1557f8a945c70016982f458ce23bdf846e0ab
Kendra Lyons <KLyons@kendra-lyonss-macbook-pro.local>
2020-09-14 18:04:27 -0700

    q3try1

--- practicum/week-2/question3.js
+++ practicum/week-2/question3.js
@@ -10,8 +10,35 @@ output: [
    [1,2,1],
   [1,3,3,1] 
 ]
+
+        if (n = 1) {
+            result.push([1]);
+        } else if (n = 2) {
+            result.push([1],[1,1]);
+        } else if (n > 2) {
+            result.push([1],[1,1]);
+            for ( j = 0; j < (n - 2); j++) {
+                var numRows = n - 2; 
+                var rows = [] * numRows;
+                result.concat(rows)
+            }
+        }
+
+help from David Fimbres 
 */
 
 var PascalTriangle = function(n) {
-    
+    var result = []
+    for (i = 0; i < n; i++) {
+        result.push([]);
+        for (j = 0; j < (i + 1); j++) {
+            if (i == 0 || i == 1 || j == 0) {
+                result[i][j].push(1);
+            }
+            else {
+                result[i][j].push((result[i-1][j-1]+result[i-1][j]));
+            }
+        }
+    }
+    return result;
 };
