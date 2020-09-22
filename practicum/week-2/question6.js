b89ce785eaf309b94dbc766c25bdd4dbeb9b27d4
Kendra Lyons <KLyons@kendra-lyonss-macbook-pro.local>
2020-09-22 16:42:34 -0700

    q6done

--- practicum/week-2/question6.js
+++ practicum/week-2/question6.js
@@ -12,16 +12,15 @@ output: 2
 */
 
 var m_element = function(input) {
-    
-    //start a for loop to traverse the array from left to right
+    //start a for loop to pick a value to count its repetitions
     for (i = 0; i < input.length; i++) {
         var count = 0;
         var compare = input[i];
-        //start another loop to compare adjacent values 
+        //start another loop to compare the selected value to all other values
         for (j = i; j < input.length; j++) {
             if (input[j] == compare) { 
                 count += 1
-                }
+            }
         }
         if (count > input.length/2) {
             return input[i];
@@ -30,5 +29,5 @@ var m_element = function(input) {
     return -1
 };
 
-// let test6 = m_element([1,2,3,4,5,6,7,8]);
-// console.log(test6)
+let test6 = m_element([1, 2, 2, 3, 2, 7, 2]);
+console.log(test6)
