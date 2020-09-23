d02b50115f2aed652846a149369d5e4219a46fd6
Kendra Lyons <KLyons@kendra-lyonss-macbook-pro.local>
2020-09-22 18:36:56 -0700

    q2

--- practicum/week-3/question2.js
+++ practicum/week-3/question2.js
@@ -11,6 +11,21 @@ Example: input: [19,3,2,10,8,2,3,5]
           */
 
  var findDuplicates = function(input) {
-     
+    var duplicates = [];
+    for (i = 0; i < input.length; i++) {
+        let compare = input[i];
+        let count = 0;
+        for (j = 0; j < input.length; j++) {
+            if (compare == input[j]) {
+                count += 1;
+            }
+        }
+        if (count == 2 && !duplicates.includes(compare)) {
+            duplicates.push(compare);
+        }
+    }
+    return duplicates;
     
  };
+test2 = findDuplicates(-17,3,1,5,-9,1,10);
+console.log(test2);
