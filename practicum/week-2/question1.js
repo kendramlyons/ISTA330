0ebe35b8ec6cc4644f68831bc8442800c39e540b
Kendra Lyons <KLyons@kendra-lyonss-macbook-pro.local>
2020-09-27 19:08:39 -0700

    p2q1 sort function

--- practicum/week-2/question1.js
+++ practicum/week-2/question1.js
@@ -10,9 +10,12 @@ is at least twice as much as every other number
 */
 
 var largestNumberIsAtLeastTwice = function(input) {
-    var sorted_in = input.sort();
+    var sorted_in = input.sort(function(a, b){return a-b});
+    console.log(sorted_in);
     const biggest = sorted_in.pop();
+    console.log(biggest);
     const next_big = sorted_in.pop();
+    console.log(next_big);
     if ((biggest/2) >= next_big) {
         return true;
     }
@@ -20,3 +23,5 @@ var largestNumberIsAtLeastTwice = function(input) {
         return false;
     }
 }; 
+test1 = largestNumberIsAtLeastTwice([3,2,21,50]);
+console.log(test1);
